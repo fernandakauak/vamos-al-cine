1dc1bd6b91292822c4434d180ce21a2147dfe116
Fernanda Kauak Alaff <fernanda.kauak@gmail.com>
2026-07-15 21:46:53 -0400

    Inicial

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup>
+import FerComp from './components/FerComp.vue';
+import MartinComp from './components/MartinComp.vue';
+</script>
+
+<template>
+  <MartinComp />
+  <FerComp />
+</template>
