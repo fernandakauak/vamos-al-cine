f05ea220fc01533d94569ed5af26da30c0eb6238
Fernanda Kauak Alaff <fernanda.kauak@gmail.com>
2026-07-17 20:13:14 -0400

    Componentes viejos eliminados

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,4 @@
 <script setup>
-import FerComp from './components/FerComp.vue';
-import MartinComp from './components/MartinComp.vue';
 import ButacasComp from './components/ButacasComp.vue';
 
 </script>
